b97a708488b81f3cfb6eb9697a00a55ec76ea7df
Nathan Cunningham <penguin2774@gmail.com>
2009-09-18 14:36:11 -0400

    added texture loading examples with SDL and the image class

--- sdl/chap7/loadimage.lisp
+++ sdl/chap7/loadimage.lisp
@@ -27,31 +27,48 @@
 
 (defvar *fire* nil)
 
+
 (defgeneric setup-RC (w))
 
 (defmethod setup-RC ((w window))
   (gl:clear-color 0.0 0.0 0.0 0.0))
 
+   
+(defun convert-image-for-gl (surface)
+  (let ((result (sdl:create-surface (sdl:width surface) (sdl:height surface)
+				    :bpp 24)))
+    (sdl:blit-surface surface result)
+    result))
+  
+(defun get-pixel-pointer (surface)
+  (cffi:foreign-slot-value (sdl::fp surface) 'sdl-cffi::SDL-Surface 'sdl-cffi::pixels))
+
+
 
 (defgeneric render-scene (w))
 (defmethod render-scene ((w window))
+  
   (gl:clear :color-buffer)
-  (gl:pixel-store :unpack-alignment 1)
-  (gl:raster-pos 0 0)
-  (gl:draw-pixels (sdl:width *fire*) (sdl:height *fire*) )
+;  (gl:pixel-store :unpack-alignment 1)
+  (gl:raster-pos  0 (sdl:height *fire*) )
+  (%gl:pixel-zoom 1.0 -1.0) ; SDL orients from the top left, where as gl orients from the bottem left, so we have to flip it. (theres gatta be a better way >.<)
+   
+  (%gl:draw-pixels (sdl:width *fire*) (sdl:height *fire*) :bgr :unsigned-byte
+		     (sdl-base::with-pixel (pixels (sdl::fp *fire*))
+		       (sdl-base::pixel-data pixels))))
+  
 
 
 (defgeneric resize-window (window w h))
 (defmethod resize-window ((win window) w h)
   (when (zerop h)
     (setf h 1))
-  (let ((ratio (float (/ w h))))
-    (gl:viewport 0 0 w h)
-    (gl:matrix-mode :projection)
-    (gl:load-identity)
-    (glu:ortho-2d 0.0 (float w) 0.0 (float h))
-    (gl:matrix-mode :modelview)
-    (gl:load-identity)))
+  (gl:viewport 0 0 w h)
+  (gl:matrix-mode :projection)
+  (gl:load-identity)
+  (glu:ortho-2d  0.0 (float w) 0.0 (float h))
+  (gl:matrix-mode :modelview)
+  (gl:load-identity))
 
 
 	
@@ -59,9 +76,8 @@
 (defun run ()
   (let ((w (make-instance 'window)))
     (sdl:with-init ()
-      (setf *fire* (sdl-image:load-image "Fire.tga" :image-type :tga))
       (sdl:window 512 512 :flags sdl:SDL-OPENGL :title-caption "Nate's amazing moving box")
-    
+      (setf *fire* (sdl-image:load-image "Fire.tga" :image-type :tga ))
       (resize-window w 512 512)
       (setup-RC w)
       (render-scene w)
--- super-bible.asd
+++ super-bible.asd
@@ -7,7 +7,7 @@
 
 
 (defsystem super-bible
-  :depends-on (cl-opengl cl-glut cl-glu)
+  :depends-on (cl-opengl cl-glut cl-glu lispbuilder-sdl lispbuilder-sdl-image)
   :components
    ((:file "math3d")
     (:file "gltools" :depends-on ("math3d"))
